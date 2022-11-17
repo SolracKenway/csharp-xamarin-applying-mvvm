1e4d8e977722d63daa4cea456ab3e75d88fe0cdd
SolracKenway <c_847@hotmail.com>
2022-11-17 16:53:14 -0700

    Module 5

---
+++ Roster.Client/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roster.Client.Models
+{
+    class Person
+    {
+    }
+}
--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
 using Xamarin.Forms;
+using Roster.Client.Models;
+using System.Collections.ObjectModel;
 
 namespace Roster.Client.ViewModels
 {
@@ -25,9 +27,15 @@ namespace Roster.Client.ViewModels
 
         public Command UpdateApplicationCommand { get; }
 
+        public ObservableCollection<Person> People { get; }
+
         public HomeViewModel ()
         {
             UpdateApplicationCommand  = new Command(UpdateApplicationExecute);
+            People = new ObservableCollection<Person>();
+            People.Add(new Person { Name = "Delores Feil", Company = "Legros Group" });
+            People.Add(new Person { Name = "Ann Zboncak", Company = "Ledner - Ferry" });
+            People.Add(new Person { Name = "Jaime Lesch", Company = "Herzog and Sons" });
         }
         private void UpdateApplicationExecute()
         {
