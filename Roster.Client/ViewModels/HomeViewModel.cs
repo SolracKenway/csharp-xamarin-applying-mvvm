ffdefb6b2e91d094a4a34e11dec3f6db745c2edc
SolracKenway <c_847@hotmail.com>
2022-11-17 16:41:13 -0700

    Module 4

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -7,11 +7,31 @@ using Xamarin.Forms;
 namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
+
+        
     {
-        public string Title { get; set; } = "Roster App";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _title = "Roster App";
+
+        public string Title {
+            get => _title;
+            set
+            {
+                _title = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
 
         public Command UpdateApplicationCommand { get; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public HomeViewModel ()
+        {
+            UpdateApplicationCommand  = new Command(UpdateApplicationExecute);
+        }
+        private void UpdateApplicationExecute()
+        {
+            Title = "Roster App (v2.0)";
+        }
     }
 }
