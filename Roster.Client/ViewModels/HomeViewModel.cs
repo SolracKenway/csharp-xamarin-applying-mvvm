1c2a33eadf503474889e696d7d641aad45e132a2
SolracKenway <c_847@hotmail.com>
2022-11-17 15:38:15 -0700

    Module 3 Complete?

---
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Roster.Client.ViewModel
+{
+    class HomeViewModel
+    {
+        public string Title { get; set; }
+
+        public Command UpdateApplicationCommand;
+    }
+}
